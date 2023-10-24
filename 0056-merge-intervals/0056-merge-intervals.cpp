cc204b86a796fa04a008b77f4f016e47450b31b1
Mohd.Ayaan Khan <101355399+khanayaan7@users.noreply.github.com>
2023-10-24 13:43:23 +0530

    Time: 31 ms (56.07%), Space: 19.3 MB (58.84%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,20 +4,17 @@ public:
         vector<vector<int>> ans;
         sort(intervals.begin(),intervals.end());
         int n=intervals.size(),curr=0;
-        vector<int> vis(n,1);
         for(int i=1;i<n;i++){
             int f=intervals[i][0];
             if(f>=intervals[curr][0] && f<=intervals[curr][1]){
                 intervals[curr][1]=max(intervals[curr][1],intervals[i][1]);
-                vis[i]=0;
             }
             else{
+                ans.push_back({intervals[curr][0],intervals[curr][1]});
                 curr=i;
             }
         }
-        for(int i=0;i<n;i++){
-            if(vis[i])ans.push_back({intervals[i][0],intervals[i][1]});
-        }
+        ans.push_back({intervals[curr][0],intervals[curr][1]});
         return ans;
     }
 };
